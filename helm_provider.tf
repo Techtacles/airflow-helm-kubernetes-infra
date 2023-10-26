518607e1aad7709afe895cb0ae4e371daccca84c
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 15:45:09 +0100

    destroying

--- helm_provider.tf
+++ helm_provider.tf
@@ -1,17 +1,17 @@
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.eks_auth.token
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.eks_auth.token
 
-  experiments {
-    manifest_resource = true
-  }
-}
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.eks_auth.token
+#   experiments {
+#     manifest_resource = true
+#   }
+# }
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#     token                  = data.aws_eks_cluster_auth.eks_auth.token
 
-  }
-}
+#   }
+# }
