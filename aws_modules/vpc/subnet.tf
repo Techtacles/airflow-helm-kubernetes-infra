1db807c8f7b66690431be01f4217dd41537e4524
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 08:44:21 +0100

    Adding public subnets, and modifying route tables

--- aws_modules/vpc/subnet.tf
+++ aws_modules/vpc/subnet.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "public_subnet" {
   count                   = length(var.subnet_availability_zones)
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr_block, 16, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
   availability_zone       = var.subnet_availability_zones[count.index]
   map_public_ip_on_launch = true
 
