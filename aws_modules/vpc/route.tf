e1e79c1fd795e6742dd4d120754dd1eae7af8baf
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 04:46:11 +0100

    Creating multiple nat gateways for multiple az

--- aws_modules/vpc/route.tf
+++ aws_modules/vpc/route.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.natgw.id
+    gateway_id = aws_nat_gateway.natgw[*].id
   }
 
 
