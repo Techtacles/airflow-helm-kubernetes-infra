837713c4ff491f9acc93d3fc30523237037f3d92
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 14:34:59 +0100

    Rebuilding resources

--- data.tf
+++ data.tf
@@ -1,6 +1,8 @@
 data "aws_eks_cluster_auth" "eks_auth" {
-  name = var.eks_cluster_name
+  name       = var.eks_cluster_name
+  depends_on = [module.eks]
 }
 data "aws_eks_cluster" "cluster" {
-  name = var.eks_cluster_name
+  name       = var.eks_cluster_name
+  depends_on = [module.eks]
 }
