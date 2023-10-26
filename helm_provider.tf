a760d6ef8652f4fcf36997bc52b3c247d4cee936
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 11:40:16 +0100

    Changing provider auth method

--- helm_provider.tf
+++ helm_provider.tf
@@ -1,16 +1,10 @@
 provider "kubernetes" {
-  host                   = module.eks.0.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.0.cluster_name]
-    command     = "aws"
-  }
+
+  config_path = "~/.kube/config"
+
 }
 provider "helm" {
   kubernetes {
-    host                   = module.eks.0.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
-
+    config_path = "~/.kube/config"
   }
 }
