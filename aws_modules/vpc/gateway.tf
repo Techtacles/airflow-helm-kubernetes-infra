d0419f19858efe00f19045d57e5a92a3dda9bad2
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 04:05:08 +0100

    Removing allocation id for nat

--- aws_modules/vpc/gateway.tf
+++ aws_modules/vpc/gateway.tf
@@ -1,5 +1,6 @@
 resource "aws_nat_gateway" "natgw" {
-  subnet_id     = aws_subnet.subnet.0.id
+  subnet_id         = aws_subnet.subnet.0.id
+  connectivity_type = "private"
   tags = {
     Name = "gw NAT"
   }
