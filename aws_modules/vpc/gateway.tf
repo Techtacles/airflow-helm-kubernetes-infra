36662de5712dce1aeb040c1e48d6a3b704e07a96
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 03:58:04 +0100

    Modifying private subnet route to internet using nat

--- aws_modules/vpc/gateway.tf
+++ aws_modules/vpc/gateway.tf
@@ -1,7 +1,5 @@
 resource "aws_nat_gateway" "natgw" {
-  allocation_id = aws_eip.lb.id
   subnet_id     = aws_subnet.subnet.0.id
-  depends_on    = [aws_internet_gateway.gw]
   tags = {
     Name = "gw NAT"
   }
