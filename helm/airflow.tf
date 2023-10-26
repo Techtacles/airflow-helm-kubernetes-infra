6c961a3f499d28a4a5048de23fede3bfd1f85aba
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 16:00:02 +0100

    Increasing timeout

--- helm/airflow.tf
+++ helm/airflow.tf
@@ -2,6 +2,8 @@ resource "helm_release" "airflow_chart" {
   name             = var.helm_release_name
   namespace        = kubernetes_namespace_v1.namespace.metadata[0].name
   create_namespace = true
+  timeout          = 1200
+  force_update     = true
 
   repository = var.helm_repo
   chart      = var.helm_chart
