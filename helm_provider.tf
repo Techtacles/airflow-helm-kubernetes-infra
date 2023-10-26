4522c8837e4927bea640e8e8cc38ad8e4d85221a
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 13:59:30 +0100

    commenting token

--- helm_provider.tf
+++ helm_provider.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
   host                   = module.eks.0.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
-  token                  = data.aws_eks_cluster_auth.eks_auth.token
+  #token                  = data.aws_eks_cluster_auth.eks_auth.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks.0.cluster_name]
@@ -12,7 +12,7 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.0.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
-    token                  = data.aws_eks_cluster_auth.eks_auth.token
+    #token                  = data.aws_eks_cluster_auth.eks_auth.token
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.0.cluster_name]
