8909b1db13f7948206f781a070c8c24b8d516b0b
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 14:02:38 +0100

    Adding manifest resource

--- helm_provider.tf
+++ helm_provider.tf
@@ -1,23 +1,21 @@
 provider "kubernetes" {
   host                   = module.eks.0.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
-  #token                  = data.aws_eks_cluster_auth.eks_auth.token
+  token                  = data.aws_eks_cluster_auth.eks_auth.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks.0.cluster_name]
     command     = "aws"
+  }
+  experiments {
+    manifest_resource = true
   }
 }
 provider "helm" {
   kubernetes {
     host                   = module.eks.0.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
-    #token                  = data.aws_eks_cluster_auth.eks_auth.token
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.0.cluster_name]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.eks_auth.token
 
   }
 }
