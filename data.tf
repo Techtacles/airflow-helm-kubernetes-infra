faf77b5c87de16014959e59a6573dcc8e95f1fc6
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 15:40:06 +0100

    Changing data from var to module

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
 data "aws_eks_cluster_auth" "eks_auth" {
-  name       = module.eks.eks_cluster_id
+  name       = module.eks.0.eks_cluster_id
   depends_on = [module.eks]
 }
 data "aws_eks_cluster" "cluster" {
-  name       = module.eks.eks_cluster_id
+  name       = module.eks.0.eks_cluster_id
   depends_on = [module.eks]
 }
