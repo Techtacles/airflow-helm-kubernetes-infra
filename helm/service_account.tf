79f88c3778ad1cfe6d5ef7aa241dd13efbe18d2d
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 21:57:28 +0100

    Adding exec to provider

--- helm/service_account.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_secret_v1" "service_acc" {
-  metadata {
-    annotations = {
-      "kubernetes.io/service-account.name" = "my-service-account"
-    }
-  }
-
-  type = "kubernetes.io/service-account-token"
-}
