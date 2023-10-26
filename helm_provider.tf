98ed98818739142fb0bb7e4a3f018681d09d9e90
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-26 12:06:41 +0100

    Adding token to provider

--- helm_provider.tf
+++ helm_provider.tf
@@ -1,10 +1,18 @@
 provider "kubernetes" {
-
-  config_path = "~/.kube/config"
-
+  host                   = module.eks.0.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
+  token                  = data.aws_eks_cluster_auth.eks_auth.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.0.cluster_name]
+    command     = "aws"
+  }
 }
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = module.eks.0.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.0.eks_cluster_certificate.0.data)
+    token                  = data.aws_eks_cluster_auth.eks_auth.token
+
   }
 }
