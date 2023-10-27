132f588fdde4d3970f4e4472bc8305f52d6c2f29
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-27 16:11:29 +0100

    Adding target to plan and apply

--- aws_provider.tf
+++ aws_provider.tf
@@ -4,10 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.22.0"
     }
-    kubernetes = {
-      source  = "registry.terraform.io/hashicorp/kubernetes"
-      version = "~> 1.1"
-    }
+    
   }
 }
 
