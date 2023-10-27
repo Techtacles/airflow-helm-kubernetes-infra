513f880cf31e0adb6e9731cd6c2be8b98df26ed4
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-27 11:14:47 +0100

    replace

---
+++ resources/kubectlinstall.sh
@@ -0,0 +1,6 @@
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+aws eks update-kubeconfig --region us-east-1 --name airflow-eks-cluster
