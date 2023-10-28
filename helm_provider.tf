cda21bac13e866174e3fb89a4165ad7c04a1741e
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-28 14:57:47 +0100

    Modifying kube provider

--- helm_provider.tf
+++ helm_provider.tf
@@ -1,9 +1,9 @@
 provider "kubernetes" {
-  config_path = "config.yaml"
+  config_path = "/home/runner/work/airflow-kafka-kubernetes-infra/airflow-kafka-kubernetes-infra/config.yaml"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "config.yaml"
+    config_path = "/home/runner/work/airflow-kafka-kubernetes-infra/airflow-kafka-kubernetes-infra/config.yaml"
   }
 }
