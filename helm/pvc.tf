36b00b5771bff129d71ad72e1011c45787c92672
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-28 15:38:03 +0100

    Adding pvc and pv

--- helm/pvc.tf
+++ helm/pvc.tf
@@ -1,34 +1,36 @@
-# resource "kubernetes_persistent_volume_claim_v1" "pvc" {
-#   #depends_on = [kubernetes_persistent_volume_v1.pv]
-#   metadata {
-#     name      = var.pvc_name
-#     namespace = kubernetes_namespace_v1.namespace.metadata[0].name
-#   }
-#   spec {
-#     access_modes = ["ReadWriteMany"]
-#     resources {
-#       requests = {
-#         storage = "5Gi"
-#       }
-#     }
-#     storage_class_name = var.storage_class_name
-#   }
-# }
+resource "kubernetes_persistent_volume_claim_v1" "pvc" {
+  depends_on = [kubernetes_persistent_volume_v1.pv]
+  metadata {
+    name      = var.pvc_name
+    namespace = kubernetes_namespace_v1.namespace.metadata[0].name
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "5Gi"
+      }
+    }
+    #storage_class_name = var.storage_class_name
+    volume_name = kubernetes_persistent_volume_v1.pv.metadata.0.name
+  }
+}
 
-/* resource "kubernetes_persistent_volume_v1" "pv" {
+resource "kubernetes_persistent_volume_v1" "pv" {
   metadata {
     name = "airflow-pv"
   }
   spec {
     capacity = {
-      storage = "10Gi"
+      storage = "5Gi"
     }
-    storage_class_name = var.storage_class_name
-    access_modes       = ["ReadWriteMany"]
+    #storage_class_name = var.storage_class_name
+    access_modes = ["ReadWriteOnce"]
     persistent_volume_source {
-      aws_elastic_block_store {
-        volume_id = var.ebs_volume
+      csi {
+        driver        = "ebs.csi.aws.com"
+        volume_handle = var.ebs_volume
       }
     }
   }
-} */
+}
