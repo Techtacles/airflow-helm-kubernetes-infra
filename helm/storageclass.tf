3aa5c4145468c52299625007613a8eb6128bdb18
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-28 16:44:14 +0100

    Adding storage class

--- helm/storageclass.tf
+++ helm/storageclass.tf
@@ -1,5 +1,5 @@
 resource "kubernetes_storage_class_v1" "storage_class" {
-  depends_on = [helm_release.ebs_csi_driver]
+  #depends_on = [helm_release.ebs_csi_driver]
   metadata {
     name = var.storage_class_name
     annotations = {
