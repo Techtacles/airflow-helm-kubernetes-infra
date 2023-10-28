feb99d60513c5956a756825495e0bffac035e921
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-28 10:24:09 +0100

    modifying helm provider

--- helm_provider.tf
+++ helm_provider.tf
@@ -1,13 +1,5 @@
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.eks_auth.token
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
-    command     = "aws"
-  }
+  config_path = "~/.kube/config"
 }
 
 provider "helm" {
