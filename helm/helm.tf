a71ed349d5bc4c8ce71159af721d78f687d42dc4
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-30 17:23:49 +0100

    modifying service

--- helm/helm.tf
+++ helm/helm.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "airflow_chart" {
-  name      = var.helm_release_name
+  name      = "${var.helm_release_name}-1"
   namespace = kubernetes_namespace.namespace.metadata[0].name
   #create_namespace = true
   timeout      = 2500
