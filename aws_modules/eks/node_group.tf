88aa90e4957b550f93a4244d7bd2102973469bfb
Techtacles <emmanuel.offisong@data2bots.com>
2023-10-30 10:00:37 +0100

    Destroyign temp

--- aws_modules/eks/node_group.tf
+++ aws_modules/eks/node_group.tf
@@ -4,8 +4,8 @@ resource "aws_eks_node_group" "node_group" {
   node_role_arn   = aws_iam_role.eks_nodegroup_iam.arn
   subnet_ids      = var.node_group_subnet_ids
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 4
     min_size     = 1
   }
 
