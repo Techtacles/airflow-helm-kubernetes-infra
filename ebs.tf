c56cc31df00b0ec5ced2c941d4032aff1ed31391
Techtacles <emmanuel.offisong@data2bots.com>
2023-11-02 08:04:13 +0100

    creating helm resources

--- ebs.tf
+++ ebs.tf
@@ -1,8 +1,8 @@
-# resource "aws_ebs_volume" "ebs_pvc" {
-#   availability_zone = "us-east-1a"
-#   size              = 10
+resource "aws_ebs_volume" "ebs_pvc" {
+  availability_zone = "us-east-1a"
+  size              = 10
 
-#   tags = {
-#     Name = "PvcEbsVolume"
-#   }
-# }
+  tags = {
+    Name = "PvcEbsVolume"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# module "eks_vpc" {
-#   count                     = var.enable_workflow == true ? 1 : 0
-#   source                    = "./aws_modules/vpc"
-#   vpc_cidr_block            = var.vpc_cidr_block
-#   tag                       = var.tag
-#   subnet_availability_zones = var.subnet_availability_zones
-#   security_group_name       = var.security_group_name
-#   subnet_tag                = var.subnet_tag
+module "eks_vpc" {
+  count                     = var.enable_workflow == true ? 1 : 0
+  source                    = "./aws_modules/vpc"
+  vpc_cidr_block            = var.vpc_cidr_block
+  tag                       = var.tag
+  subnet_availability_zones = var.subnet_availability_zones
+  security_group_name       = var.security_group_name
+  subnet_tag                = var.subnet_tag
 
-# }
+}
 
 module "rds_vpc" {
   count                     = var.enable_workflow == true ? 1 : 0
@@ -35,60 +35,60 @@ module "rds" {
   vpc_id               = module.rds_vpc.0.vpc_id
 }
 
-# module "eks" {
-#   count                 = var.enable_workflow == true ? 1 : 0
-#   source                = "./aws_modules/eks"
-#   depends_on            = [module.eks_vpc]
-#   eks_iam_role_name     = var.eks_iam_role_name
-#   eks_cluster_name      = var.eks_cluster_name
-#   subnet_id_list        = flatten([module.eks_vpc.0.public_subnet_ids[*]])
-#   node_group_iam_name   = var.node_group_iam_name
-#   node_group_name       = var.node_group_name
-#   node_group_subnet_ids = flatten(module.eks_vpc.0.public_subnet_ids[*])
-#   oidc_iam_role_name    = var.oidc_iam_role_name
-#   namespace_name        = var.namespace_name
-#   service_acc_name      = var.service_acc_name
-#   oidc_policy_name      = var.oidc_policy_name
+module "eks" {
+  count                 = var.enable_workflow == true ? 1 : 0
+  source                = "./aws_modules/eks"
+  depends_on            = [module.eks_vpc]
+  eks_iam_role_name     = var.eks_iam_role_name
+  eks_cluster_name      = var.eks_cluster_name
+  subnet_id_list        = flatten([module.eks_vpc.0.public_subnet_ids[*]])
+  node_group_iam_name   = var.node_group_iam_name
+  node_group_name       = var.node_group_name
+  node_group_subnet_ids = flatten(module.eks_vpc.0.public_subnet_ids[*])
+  oidc_iam_role_name    = var.oidc_iam_role_name
+  namespace_name        = var.namespace_name
+  service_acc_name      = var.service_acc_name
+  oidc_policy_name      = var.oidc_policy_name
 
+}
+# resource "null_resource" "update_config" {
+#   depends_on = [module.eks]
+#   triggers = {
+#     always = timestamp()
+#   }
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       aws eks update-kubeconfig --region us-east-1 --name ${var.eks_cluster_name}
+#       cat /home/runner/.kube/config >> config.yaml
+#       ls -al
+#       pwd
+#     EOT
+#   }
 # }
-# # resource "null_resource" "update_config" {
-# #   depends_on = [module.eks]
-# #   triggers = {
-# #     always = timestamp()
-# #   }
-# #   provisioner "local-exec" {
-# #     command = <<EOT
-# #       aws eks update-kubeconfig --region us-east-1 --name ${var.eks_cluster_name}
-# #       cat /home/runner/.kube/config >> config.yaml
-# #       ls -al
-# #       pwd
-# #     EOT
-# #   }
-# # }
 
-# module "airflow_helm_chart" {
-#   count              = var.enable_workflow == true ? 1 : 0
-#   source             = "./helm"
-#   namespace_name     = var.namespace_name
-#   helm_release_name  = var.helm_release_name
-#   helm_repo          = var.helm_repo
-#   helm_chart         = var.helm_chart
-#   db_host            = module.rds.0.db_address
-#   db_port            = module.rds.0.db_port
-#   db_name            = module.rds.0.db_name
-#   db_user            = var.rds_username
-#   db_password        = var.rds_password
-#   service_acc_name   = var.service_acc_name
-#   eks_oidc_arn       = module.eks.0.eks_oicd_arn
-#   secret_name        = var.secret_name
-#   pvc_name           = var.pvc_name
-#   storage_class_name = var.storage_class_name
-#   svc_name           = var.svc_name
-#   airflow_email      = var.airflow_email
-#   ingress_name       = var.ingress_name
-#   ebs_volume         = aws_ebs_volume.ebs_pvc.id
-#   rds_address        = module.rds.0.db_address
-#   eks_cluster_name   = module.eks.0.cluster_name
+module "airflow_helm_chart" {
+  count              = var.enable_workflow == true ? 1 : 0
+  source             = "./helm"
+  namespace_name     = var.namespace_name
+  helm_release_name  = var.helm_release_name
+  helm_repo          = var.helm_repo
+  helm_chart         = var.helm_chart
+  db_host            = module.rds.0.db_address
+  db_port            = module.rds.0.db_port
+  db_name            = module.rds.0.db_name
+  db_user            = var.rds_username
+  db_password        = var.rds_password
+  service_acc_name   = var.service_acc_name
+  eks_oidc_arn       = module.eks.0.eks_oicd_arn
+  secret_name        = var.secret_name
+  pvc_name           = var.pvc_name
+  storage_class_name = var.storage_class_name
+  svc_name           = var.svc_name
+  airflow_email      = var.airflow_email
+  ingress_name       = var.ingress_name
+  ebs_volume         = aws_ebs_volume.ebs_pvc.id
+  rds_address        = module.rds.0.db_address
+  eks_cluster_name   = module.eks.0.cluster_name
 
 
-# }
+}
