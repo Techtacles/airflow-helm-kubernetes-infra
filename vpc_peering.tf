e94356c60437822be125bd09106449987d918251
Techtacles <emmanuel.offisong@data2bots.com>
2023-11-02 07:34:38 +0100

    Testing public conn to rds

--- vpc_peering.tf
+++ vpc_peering.tf
@@ -1,17 +1,17 @@
-resource "aws_vpc_peering_connection" "vpc_peering" {
-  count       = var.enable_workflow == true ? 1 : 0
-  peer_vpc_id = module.rds_vpc.0.vpc_id
-  vpc_id      = module.eks_vpc.0.vpc_id
-  auto_accept = true
-  tags = {
-    Side = "VPC Peering"
-  }
-  depends_on = [module.eks_vpc, module.rds_vpc]
-}
+# resource "aws_vpc_peering_connection" "vpc_peering" {
+#   count       = var.enable_workflow == true ? 1 : 0
+#   peer_vpc_id = module.rds_vpc.0.vpc_id
+#   vpc_id      = module.eks_vpc.0.vpc_id
+#   auto_accept = true
+#   tags = {
+#     Side = "VPC Peering"
+#   }
+#   depends_on = [module.eks_vpc, module.rds_vpc]
+# }
 
-resource "aws_route" "peering_routes" {
-  route_table_id            = module.eks_vpc.0.route_table_id
-  destination_cidr_block    = module.rds_vpc.0.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.0.id
-  depends_on                = [aws_vpc_peering_connection.vpc_peering]
-}
+# resource "aws_route" "peering_routes" {
+#   route_table_id            = module.eks_vpc.0.route_table_id
+#   destination_cidr_block    = module.rds_vpc.0.vpc_cidr
+#   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.0.id
+#   depends_on                = [aws_vpc_peering_connection.vpc_peering]
+# }
